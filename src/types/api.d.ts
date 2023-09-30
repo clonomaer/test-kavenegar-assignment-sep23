4a6de39c7b62d5a0bd30ce05cbe761ea62a2ddea
leila_ilkhani <work@leilynn.link>
2023-10-01 02:06:33 +0330

    add new QueryOptions type helper

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,8 +1,21 @@
+import { UseQueryOptions, QueryKey } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse } from "axios";
+
+export type DataBaseType = Record<string, unknown> | Record<string, unknown>[];
+
 /** @desc could carry status and errors if this API was not mocked */
-export type ApiResponse<
-  Data extends Record<string, unknown> | Record<string, unknown>[]
-> = {
+export type ApiResponse<Data extends DataBaseType> = {
   status: number;
   errors: string[] | null;
   data: Data;
 };
+
+export type QueryOptions<
+  TData extends DataBaseType,
+  TQueryKey extends QueryKey = QueryKey
+> = UseQueryOptions<
+  AxiosResponse<ApiResponse<TData>>,
+  AxiosError,
+  TData,
+  TQueryKey
+>;

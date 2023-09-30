1b6d4c678a61c90b483be09b5ba508a0f9686531
leila_ilkhani <work@leilynn.link>
2023-09-30 15:09:01 +0330

    add cookies utilities

---
+++ src/utils/cookie.ts
@@ -0,0 +1,21 @@
+export function getCookie(name: string): string | undefined {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    return parts.pop()?.split(";").shift();
+  }
+}
+
+export function removeCookie(name: string): void {
+  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+}
+
+export function setCookie(name: string, value: string, days: number): void {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = `; expires=${date.toUTCString()}`;
+  }
+  document.cookie = `${name}=${value || ""}${expires}; path=/`;
+}

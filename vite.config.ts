4d25a2d1c897afcde4b16b095257d391c3f21a11
leila_ilkhani <work@leilynn.link>
2023-09-30 13:08:09 +0330

    mod fixing baseurl problems with github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import type {} from "./src/vite-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/test-kavenegar-assignment-sep23/",
+  base: process.env.VITE_BASE_URL,
 });

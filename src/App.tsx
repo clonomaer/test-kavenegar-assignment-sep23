ba8f1c3e2b388bae883125c255fe3a2ed0e48f68
leila_ilkhani <work@leilynn.link>
2023-09-30 17:37:18 +0330

    add react-query base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import ApplicationRouter from "./routes/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ApplicationRouter />
-    </>
+    </QueryClientProvider>
   );
 }
 

c66b668014fc87dd8a3b0d5b82790ad3ca67a096
leila_ilkhani <work@leilynn.link>
2023-10-01 08:32:53 +0330

    add back button and notFound message for TicketDetails

--- src/components/SingleTicketMessageList.tsx
+++ src/components/SingleTicketMessageList.tsx
@@ -6,6 +6,8 @@ import LoadingSpinner from "./LoadingSpinner";
 import { defaultLocale } from "../locales/default";
 import _ from "lodash";
 import { formatLocalDateTime, parseServerDate } from "../utils/format-datetime";
+import Button from "./Button";
+import { Link } from "react-router-dom";
 
 interface TicketDetailsProps {
   ticketId: TicketData["id"];
@@ -30,27 +32,39 @@ const TicketDetails: React.FC<TicketDetailsProps> = ({ ticketId }) => {
     return <LoadingSpinner />;
   }
 
-  return (
-    <div className="bg-dark p-4 mb-4 rounded-md">
-      <h2 className="text-text-primary">{data?.ticket.title}</h2>
-      <p className="text-text-secondary flex gap-2">
-        <span>
-          {defaultLocale.api.tickets.titles.received}
-          {`:`}
-        </span>
-        <span>
-          {formatLocalDateTime(parseServerDate(data.ticket.received))}
-        </span>
-      </p>
-      <p className="text-text-secondary flex gap-2">
-        <span>
-          {defaultLocale.api.tickets.titles.status}
-          {`:`}
-        </span>
-        <span>{defaultLocale.api.tickets.status[data.ticket.status]}</span>
-      </p>
+  if (_.isNull(data)) {
+    return <div>{defaultLocale.main.notFound}</div>;
+  }
 
-      <div className="mt-4 flex flex-col gap-4">
+  return (
+    <div className="p-4">
+      <div className="flex gap-4">
+        <Link to={"/"} className="flex">
+          <Button>{`<`}</Button>
+        </Link>
+
+        <div>
+          <h2 className="text-text-primary">{data?.ticket.title}</h2>
+          <p className="text-text-secondary flex gap-2">
+            <span>
+              {defaultLocale.api.tickets.titles.received}
+              {`:`}
+            </span>
+            <span>
+              {formatLocalDateTime(parseServerDate(data.ticket.received))}
+            </span>
+          </p>
+          <p className="text-text-secondary flex gap-2">
+            <span>
+              {defaultLocale.api.tickets.titles.status}
+              {`:`}
+            </span>
+            <span>{defaultLocale.api.tickets.status[data.ticket.status]}</span>
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-4 flex flex-col gap-2">
         <h3 className="text-text-primary">
           {defaultLocale.api.tickets.titles.messages}
         </h3>

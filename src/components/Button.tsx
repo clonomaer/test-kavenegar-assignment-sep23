56b3a1fa0c8221e9526ee425c2e10b04eb17e88f
leila_ilkhani <work@leilynn.link>
2023-10-01 06:43:02 +0330

    fix passing a classname to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,10 +4,13 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   //
 }
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
+const Button: React.FC<ButtonProps> = ({ children, className, ...rest }) => {
   return (
     <button
-      className="bg-primary-500 text-text-primary px-4 py-2 rounded hover:bg-primary-600 focus:outline-none focus:ring focus:border-primary-300"
+      className={
+        "bg-primary-500 text-text-primary px-4 py-2 rounded hover:bg-primary-600 focus:outline-none focus:ring focus:border-primary-300 " +
+        className
+      }
       {...rest}
     >
       {children}

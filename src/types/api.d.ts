a04a5d539eca1f87b9962c8f1e0abdf623ce6a1c
leila_ilkhani <work@leilynn.link>
2023-10-01 07:25:49 +0330

    fix make null data ApiResponse possible for mutation responses

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -4,7 +4,7 @@ import { AxiosError, AxiosResponse } from "axios";
 export type DataBaseType = Record<string, unknown> | Record<string, unknown>[];
 
 /** @desc could carry status and errors if this API was not mocked */
-export type ApiResponse<Data extends DataBaseType> = {
+export type ApiResponse<Data extends DataBaseType | null> = {
   status: number;
   errors: string[] | null;
   data: Data;

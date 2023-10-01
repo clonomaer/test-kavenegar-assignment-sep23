4ce5f0adc84dc9614dcf2a6e3207ad784c82c65d
leila_ilkhani <work@leilynn.link>
2023-10-01 08:15:22 +0330

    nit add some more validations for submitting ticket

--- src/types/ticket.ts
+++ src/types/ticket.ts
@@ -1,5 +1,5 @@
 import { TicketMessageData } from "./ticket-message";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export enum TicketStatus {
   "PENDING" = "pending",
@@ -23,10 +23,14 @@ export type TicketWithMessagesData = {
 /** @abstract do not instantiate directly or assign right after instantiation */
 export class TicketFormDTO {
   @IsNotEmpty({ message: "Title is required" })
+  @MinLength(6, { message: "Title is too short" })
+  @MaxLength(160, { message: "Title is too long" })
   @IsString({ message: "Title must be a string" })
   title!: string;
 
   @IsNotEmpty({ message: "Message is required" })
+  @MinLength(20, { message: "Message is too short" })
+  @MaxLength(2500, { message: "Message is too long" })
   @IsString({ message: "Message must be a string" })
   message!: string;
 }

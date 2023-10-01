c4ae6dbad0ab4e4ada3f49e8d9cec3561ffda223
leila_ilkhani <work@leilynn.link>
2023-10-01 07:27:02 +0330

    fix make logo redirect to index page

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MainHeader: React.FC = () => {
   return (
     <header className="bg-primary-500 p-4 text-white fixed top-0 w-full h-20 flex items-center">
-      <h1 className="text-2xl font-serif">Ticketing System</h1>
+      <Link to="/">
+        <h1 className="text-2xl font-serif">Ticketing System</h1>
+      </Link>
     </header>
   );
 };

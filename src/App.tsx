5240f35cf497babea975a6d11c2d644fb25baa9e
leila_ilkhani <work@leilynn.link>
2023-10-01 08:32:13 +0330

    add react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import ApplicationRouter from "./routes/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./helpers/axios-mock";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.min.css";
 
 const queryClient = new QueryClient();
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ApplicationRouter />
+      <ToastContainer />
     </QueryClientProvider>
   );
 }

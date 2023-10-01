1b2499159b3095a55b1f5df87a9c486774463bf9
leila_ilkhani <work@leilynn.link>
2023-10-01 08:38:01 +0330

    add sort tickets from new to old

--- src/services/tickets.ts
+++ src/services/tickets.ts
@@ -2,6 +2,7 @@ import { axios } from "../helpers/axios";
 import { MutationOptions, QueryOptions } from "../types/api";
 import { TicketData, TicketFormDTO } from "../types/ticket";
 import { useState } from "react";
+import { parseServerDate } from "../utils/format-datetime";
 
 export class TicketService {
   submitNewTicket() {
@@ -17,7 +18,12 @@ export class TicketService {
   readonly queryOptions: QueryOptions<TicketData[]> = {
     queryKey: ["userTicketsList"],
     queryFn: ({ signal }) => axios.get("/tickets", { signal }),
-    select: (res) => res.data.data,
+    select: (res) =>
+      res.data.data.sort(
+        (a, b) =>
+          Number(parseServerDate(b.received)) -
+          Number(parseServerDate(a.received))
+      ),
   };
 }
 
